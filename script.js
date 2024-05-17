b9139f3b3447603c0d8c0db9c8dcca827eb414b4
Nabin Jana <120771456+nabinjana-dsc@users.noreply.github.com>
2024-05-17 21:06:00 +0530

    files are added

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container"),
+        percent = document.querySelector("#percent");
+      let perVal = 0;
+      let increament = setInterval(() => {
+        perVal++;
+        percent.textContent = `${perVal}%`;
+        if (perVal == 100) {
+          clearInterval(increament);
+          container.classList.remove("active");
+        }
+      }, 100);
